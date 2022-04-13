3e830c7991dbce8abb2d7456f1953c874527e1b0
Don Wijesinghe <menuwan.wijesinghe@gmail.com>
2022-04-13 18:44:37 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Don wijesinghe on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
